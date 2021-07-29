e41a780828ff0f51d1adb4d957cb42605b0f2e1a
Samarth Shridhar <sam.shridhar1950f@gmail.com>
2021-07-28 22:55:15 -0400

    automation without loop

--- VisualizingSceneSemantics/ViewController.swift
+++ VisualizingSceneSemantics/ViewController.swift
@@ -108,6 +108,7 @@ class ViewController: UIViewController, ARSessionDelegate {
             if let result = arView.raycast(from: location, allowing: .estimatedPlane, alignment: .any).first {
                 
                 // ...
+               
                 // 2. Visualize the intersection point of the ray with the real-world surface.
                 let resultAnchor = AnchorEntity(world: result.worldTransform)
                 resultAnchor.addChild(sphere(radius: 0.01, color: .lightGray))
@@ -118,6 +119,7 @@ class ViewController: UIViewController, ARSessionDelegate {
                 
                 nearbyFaceWithClassification(to: result.worldTransform.position) { (centerOfFace, classification) in print(classification)
                     // ...
+                    DispatchQueue.main.async {
                     print("waa")
                         print("something")
                         // 4. Compute a position for the text which is near the result location, but offset 10 cm
@@ -126,19 +128,19 @@ class ViewController: UIViewController, ARSessionDelegate {
                         let textPositionInWorldCoordinates = result.worldTransform.position - (rayDirection * 0.1)
                         
                         // 5. Create a 3D text to visualize the classification result.
-                        let textEntity = self.model(for: classification)
+                        self.model(for: classification)
 
                         // 6. Scale the text depending on the distance, such that it always appears with
                         //    the same size on screen.
-                        let raycastDistance = distance(result.worldTransform.position, self.arView.cameraTransform.translation)
-                        textEntity.scale = .one * raycastDistance
+//                        let raycastDistance = distance(result.worldTransform.position, self.arView.cameraTransform.translation)
+//                        textEntity.scale = .one * raycastDistance
 
                         // 7. Place the text, facing the camera.
                         var resultWithCameraOrientation = self.arView.cameraTransform
                         resultWithCameraOrientation.translation = textPositionInWorldCoordinates
-                        let textAnchor = AnchorEntity(world: resultWithCameraOrientation.matrix)
-                        textAnchor.addChild(textEntity)
-                        self.arView.scene.addAnchor(textAnchor, removeAfter: 3)
+//                        let textAnchor = AnchorEntity(world: resultWithCameraOrientation.matrix)
+//                        textAnchor.addChild(textEntity)
+//                        self.arView.scene.addAnchor(textAnchor, removeAfter: 3)
 
                         // 8. Visualize the center of the face (if any was found) for three seconds.
                         //    It is possible that this is nil, e.g. if there was no face close enough to the tap location.
@@ -146,7 +148,7 @@ class ViewController: UIViewController, ARSessionDelegate {
                             let faceAnchor = AnchorEntity(world: centerOfFace)
                             faceAnchor.addChild(self.sphere(radius: 0.01, color: classification.color))
                             self.arView.scene.addAnchor(faceAnchor, removeAfter: 3)
-                        
+                        }
                     }
                 }
             } else {
@@ -159,56 +161,56 @@ class ViewController: UIViewController, ARSessionDelegate {
     /// Places virtual-text of the classification at the touch-location's real-world intersection with a mesh.
     /// Note - because classification of the tapped-mesh is retrieved asynchronously, we visualize the intersection
     /// point immediately to give instant visual feedback of the tap.
-    @objc
-    func handleTap(_ sender: UITapGestureRecognizer) {
-        // 1. Perform a ray cast against the mesh.
-        // Note: Ray-cast option ".estimatedPlane" with alignment ".any" also takes the mesh into account.
-        let tapLocation = sender.location(in: arView)
-        print("tap location: ")
-        print(tapLocation)
-        if let result = arView.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .any).first {
-            // ...
-            // 2. Visualize the intersection point of the ray with the real-world surface.
-            let resultAnchor = AnchorEntity(world: result.worldTransform)
-            resultAnchor.addChild(sphere(radius: 0.01, color: .lightGray))
-            arView.scene.addAnchor(resultAnchor, removeAfter: 3)
-
-            // 3. Try to get a classification near the tap location.
-            //    Classifications are available per face (in the geometric sense, not human faces).
-            nearbyFaceWithClassification(to: result.worldTransform.position) { (centerOfFace, classification) in
-                // ...
-                DispatchQueue.main.async {
-                    // 4. Compute a position for the text which is near the result location, but offset 10 cm
-                    // towards the camera (along the ray) to minimize unintentional occlusions of the text by the mesh.
-                    let rayDirection = normalize(result.worldTransform.position - self.arView.cameraTransform.translation)
-                    let textPositionInWorldCoordinates = result.worldTransform.position - (rayDirection * 0.1)
-                    
-                    // 5. Create a 3D text to visualize the classification result.
-                    let textEntity = self.model(for: classification)
-
-                    // 6. Scale the text depending on the distance, such that it always appears with
-                    //    the same size on screen.
-                    let raycastDistance = distance(result.worldTransform.position, self.arView.cameraTransform.translation)
-                    textEntity.scale = .one * raycastDistance
-
-                    // 7. Place the text, facing the camera.
-                    var resultWithCameraOrientation = self.arView.cameraTransform
-                    resultWithCameraOrientation.translation = textPositionInWorldCoordinates
-                    let textAnchor = AnchorEntity(world: resultWithCameraOrientation.matrix)
-                    textAnchor.addChild(textEntity)
-                    self.arView.scene.addAnchor(textAnchor, removeAfter: 3)
-
-                    // 8. Visualize the center of the face (if any was found) for three seconds.
-                    //    It is possible that this is nil, e.g. if there was no face close enough to the tap location.
-                    if let centerOfFace = centerOfFace {
-                        let faceAnchor = AnchorEntity(world: centerOfFace)
-                        faceAnchor.addChild(self.sphere(radius: 0.01, color: classification.color))
-                        self.arView.scene.addAnchor(faceAnchor, removeAfter: 3)
-                    }
-                }
-            }
-        }
-    }
+//    @objc
+//    func handleTap(_ sender: UITapGestureRecognizer) {
+//        // 1. Perform a ray cast against the mesh.
+//        // Note: Ray-cast option ".estimatedPlane" with alignment ".any" also takes the mesh into account.
+//        let tapLocation = sender.location(in: arView)
+//        print("tap location: ")
+//        print(tapLocation)
+//        if let result = arView.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .any).first {
+//            // ...
+//            // 2. Visualize the intersection point of the ray with the real-world surface.
+//            let resultAnchor = AnchorEntity(world: result.worldTransform)
+//            resultAnchor.addChild(sphere(radius: 0.01, color: .lightGray))
+//            arView.scene.addAnchor(resultAnchor, removeAfter: 3)
+//
+//            // 3. Try to get a classification near the tap location.
+//            //    Classifications are available per face (in the geometric sense, not human faces).
+//            nearbyFaceWithClassification(to: result.worldTransform.position) { (centerOfFace, classification) in
+//                // ...
+//                DispatchQueue.main.async {
+//                    // 4. Compute a position for the text which is near the result location, but offset 10 cm
+//                    // towards the camera (along the ray) to minimize unintentional occlusions of the text by the mesh.
+//                    let rayDirection = normalize(result.worldTransform.position - self.arView.cameraTransform.translation)
+//                    let textPositionInWorldCoordinates = result.worldTransform.position - (rayDirection * 0.1)
+//
+//                    // 5. Create a 3D text to visualize the classification result.
+//                    let textEntity = self.model(for: classification)
+//
+//                    // 6. Scale the text depending on the distance, such that it always appears with
+//                    //    the same size on screen.
+//                    let raycastDistance = distance(result.worldTransform.position, self.arView.cameraTransform.translation)
+//                    textEntity.scale = .one * raycastDistance
+//
+//                    // 7. Place the text, facing the camera.
+//                    var resultWithCameraOrientation = self.arView.cameraTransform
+//                    resultWithCameraOrientation.translation = textPositionInWorldCoordinates
+//                    let textAnchor = AnchorEntity(world: resultWithCameraOrientation.matrix)
+//                    textAnchor.addChild(textEntity)
+//                    self.arView.scene.addAnchor(textAnchor, removeAfter: 3)
+//
+//                    // 8. Visualize the center of the face (if any was found) for three seconds.
+//                    //    It is possible that this is nil, e.g. if there was no face close enough to the tap location.
+//                    if let centerOfFace = centerOfFace {
+//                        let faceAnchor = AnchorEntity(world: centerOfFace)
+//                        faceAnchor.addChild(self.sphere(radius: 0.01, color: classification.color))
+//                        self.arView.scene.addAnchor(faceAnchor, removeAfter: 3)
+//                    }
+//                }
+//            }
+//        }
+//    }
     
     @IBAction func resetButtonPressed(_ sender: Any) {
         if let configuration = arView.session.configuration {
@@ -309,7 +311,7 @@ class ViewController: UIViewController, ARSessionDelegate {
         }
     }
         
-    func model(for classification: ARMeshClassification) -> ModelEntity { // Replace this code with audio call out algorithm
+    func model(for classification: ARMeshClassification) -> Void { // Replace this code with audio call out algorithm
         // Return cached model if available
         // dimensions: 256 x 192
         guard let depthData = arView.session.currentFrame?.sceneDepth?.depthMap else { fatalError("Wut Da Dab") }
@@ -345,19 +347,19 @@ class ViewController: UIViewController, ARSessionDelegate {
 
             
             // synthesizer.continueSpeaking() // Resume a paused speech
-            return model.clone(recursive: true)
+//            return model.clone(recursive: true)
         }
         
         // Generate 3D text for the classification
-        let lineHeight: CGFloat = 0.05
-        let font = MeshResource.Font.systemFont(ofSize: lineHeight)
-        let textMesh = MeshResource.generateText(classification.description, extrusionDepth: Float(lineHeight * 0.1), font: font)
-        let textMaterial = SimpleMaterial(color: classification.color, isMetallic: true)
-        let model = ModelEntity(mesh: textMesh, materials: [textMaterial])
-        // Move text geometry to the left so that its local origin is in the center
-        model.position.x -= model.visualBounds(relativeTo: nil).extents.x / 2
-        // Add model to cache
-        modelsForClassification[classification] = model
+//        let lineHeight: CGFloat = 0.05
+//        let font = MeshResource.Font.systemFont(ofSize: lineHeight)
+//        let textMesh = MeshResource.generateText(classification.description, extrusionDepth: Float(lineHeight * 0.1), font: font)
+//        let textMaterial = SimpleMaterial(color: classification.color, isMetallic: true)
+//        let model = ModelEntity(mesh: textMesh, materials: [textMaterial])
+//        // Move text geometry to the left so that its local origin is in the center
+//        model.position.x -= model.visualBounds(relativeTo: nil).extents.x / 2
+//        // Add model to cache
+//        modelsForClassification[classification] = model
         
         if classification.description != "None" {
         let utterance = AVSpeechUtterance(string: classification.description + "at" + String(distanceAtXYPoint) + "meters")
@@ -372,7 +374,7 @@ class ViewController: UIViewController, ARSessionDelegate {
         // synthesizer.continueSpeaking() // Resume a paused speech
         }
         
-        return model
+//        return model
     }
     
     func sphere(radius: Float, color: UIColor) -> ModelEntity {
