9bb24140076da9c61e4b028f6194dde4c12346d3
Samarth Shridhar <sam.shridhar1950f@gmail.com>
2021-10-17 16:22:43 -0400

    ml model, timing fixes i think?

--- VisualizingSceneSemantics/ViewController.swift
+++ VisualizingSceneSemantics/ViewController.swift
@@ -185,7 +185,7 @@ class ViewController: UIViewController, ARSessionDelegate {
                  var classificationRequest: VNCoreMLRequest? = {
                     do {
                     
-                        let model = try VNCoreMLModel(for: ahh2().model)
+                        let model = try VNCoreMLModel(for: mod1().model)
                         var classificationList: [String] = []
                     let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
                         self?.processClassifications(for: request, error: error, completionHandler: {classification in
